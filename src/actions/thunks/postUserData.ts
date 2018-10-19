315328884e21ae01b63a0c48fe0da60897e24e6e
Laurent Bouhnik <laurent.bouhnik@justgiving.com>
2018-10-19 16:52:33 +0100

    connected the graphql database

--- src/actions/thunks/postUserData.ts
+++ src/actions/thunks/postUserData.ts
@@ -1,23 +1,24 @@
 import axios from "axios";
 
-const gpostUserData = () => {
+const postUserData = () => {
   // const body = JSON.stringify({
   //   title: "title 3",
   //   author: "author 3"
   // });
   return (
-    axios
-      .post(`http://localhost:5656/api/Books/`, {
-        mode: "no-cors",
-        headers: { "Content-Type": "application/json" },
-        body: {
-          title: "title 3",
-          author: "author 3"
-        }
-      })
+    axios({
+      method: "post",
+      url: "http://localhost:5656/api/Books/",
+      // mode: "no-cors",
+      headers: { "Content-Type": "application/json" },
+      data: {
+        title: "title 3",
+        author: "author 3"
+      }
+    })
       // .then(res => res.json())
       .then(data => console.log(data), err => console.log(err))
   );
 };
 
-export default gpostUserData;
+export default postUserData;
--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,16 +1,45 @@
 import * as React from "react";
 import getUserData from "../../actions/thunks/getUserData";
 import postUserData from "../../actions/thunks/postUserData";
+import Axios from "axios";
 
 export class UserCard extends React.Component {
-
   handleGet = () => {
     getUserData();
   };
-
   handlePost = () => {
     postUserData();
   };
+
+  async handleSubmit() {
+    const query = `mutation {
+      addBook (title: "new title", author: "new author") {
+        _id
+        title
+        author
+      }
+    }`;
+
+    // const option = {
+    //   method: "POST",
+    //   headers: { "Content-Type": "application/graphql" },
+    //   body: query
+    // };
+
+    const res = await Axios.create({
+      baseURL: "http://localhost:4000/graphql",
+      headers: { "Content-Type": "application/graphql" }
+    }).post("", query);
+    const json = await res;
+    console.log(json);
+
+    // const newBook = json.data.addBook;
+    // const books = this.state.books;
+    // const newBooks = books.concat(newBook);
+
+    // this.setState({ books: newBooks });
+  }
+
   render() {
     return (
       <>
@@ -18,7 +47,7 @@ export class UserCard extends React.Component {
         <h2>User's picture></h2>
         <h3>User's information</h3>
         <button onClick={this.handleGet}>Get Data</button>
-        <button onClick={this.handlePost}>Post Me</button>
+        <button onClick={this.handleSubmit}>Post Me</button>
       </>
     );
   }
